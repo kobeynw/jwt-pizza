d051f87afb1762185e54aa1df273cb16e6637800
Kobey Workman <kobeyworkman1@gmail.com>
2025-10-13 20:59:02 -0600

    Add Users table to admin dashboard populated with dummy data

--- src/views/adminDashboard.tsx
+++ src/views/adminDashboard.tsx
@@ -16,6 +16,28 @@ export default function AdminDashboard(props: Props) {
   const [franchiseList, setFranchiseList] = React.useState<FranchiseList>({ franchises: [], more: false });
   const [franchisePage, setFranchisePage] = React.useState(0);
   const filterFranchiseRef = React.useRef<HTMLInputElement>(null);
+  const usersList = {
+    users: [
+      {
+        id: '3',
+        name: 'Kai Chen',
+        email: 'd@jwt.com',
+        roles: [{ role: Role.Diner }],
+      },
+      {
+        id: '5',
+        name: 'Frank',
+        email: 'f@jwt.com',
+        roles: [{ role: Role.Franchisee }],
+      },
+      {
+        id: '7',
+        name: 'Johnny Test',
+        email: 'a@jwt.com',
+        roles: [{ role: Role.Admin }, { role: Role.Diner }],
+      },
+    ]
+  }
 
   React.useEffect(() => {
     (async () => {
@@ -119,9 +141,69 @@ export default function AdminDashboard(props: Props) {
               </div>
             </div>
           </div>
-        </div>
-        <div>
-          <Button className="w-36 text-xs sm:text-sm sm:w-64" title="Add Franchise" onPress={createFranchise} />
+          <div>
+            <Button className="w-36 text-xs sm:text-sm sm:w-64" title="Add Franchise" onPress={createFranchise} />
+          </div>
+          <br/>
+          <h3 className="text-neutral-100 text-xl">Users</h3>
+          <div className="bg-neutral-100 overflow-clip my-4">
+            <div className="flex flex-col">
+              <div className="-m-1.5 overflow-x-auto">
+                <div className="p-1.5 min-w-full inline-block align-middle">
+                  <div className="overflow-hidden">
+                    <table className="min-w-full divide-y divide-gray-200">
+                      <thead className="uppercase text-neutral-100 bg-slate-400 border-b-2 border-gray-500">
+                        <tr>
+                          {['Name', 'Email', 'Role', 'Action'].map((header) => (
+                            <th key={header} scope="col" className="px-6 py-3 text-center text-xs font-medium">
+                              {header}
+                            </th>
+                          ))}
+                        </tr>
+                      </thead>
+                      {usersList.users.map((user, uindex) => {
+                        return (
+                          <tbody key={uindex} className="divide-y divide-gray-200">
+                            <tr className="border-neutral-500 border-t-2">
+                              <td className="text-start px-2 whitespace-nowrap text-l font-mono text-orange-600">{user.name}</td>
+                              <td className="text-start px-2 whitespace-nowrap text-sm font-normal text-gray-800">{user.email}</td>
+                              <td className="text-start px-2 whitespace-nowrap text-sm font-normal text-gray-800">
+                                {user.roles?.map((r) => r.role).join(', ')}
+                              </td>
+                              <td className="px-6 py-1 whitespace-nowrap text-end text-sm font-medium">
+                                <button type="button" className="px-2 py-1 inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-1 border-orange-400 text-orange-400  hover:border-orange-800 hover:text-orange-800" onClick={() => console.log("Deleting User")}>
+                                  <TrashIcon />
+                                  Delete
+                                </button>
+                              </td>
+                            </tr>
+                          </tbody>
+                        );
+                      })}
+                      <tfoot>
+                        <tr>
+                          <td className="px-1 py-1">
+                            <input type="text" ref={null} name="filterUsers" placeholder="Filter users" className="px-2 py-1 text-sm border border-gray-300 rounded-lg" />
+                            <button type="submit" className="ml-2 px-2 py-1 text-sm font-semibold rounded-lg border border-orange-400 text-orange-400 hover:border-orange-800 hover:text-orange-800" onClick={() => console.log("Filtering users")}>
+                              Submit
+                            </button>
+                          </td>
+                          <td colSpan={4} className="text-end text-sm font-medium">
+                            <button className="w-12 p-1 text-sm font-semibold rounded-lg border border-transparent bg-white text-grey border-grey m-1 hover:bg-orange-200 disabled:bg-neutral-300 " onClick={() => console.log("Paginating back")} disabled={true}>
+                              «
+                            </button>
+                            <button className="w-12 p-1 text-sm font-semibold rounded-lg border border-transparent bg-white text-grey border-grey m-1 hover:bg-orange-200 disabled:bg-neutral-300" onClick={() => console.log("Paginating forward")} disabled={false}>
+                              »
+                            </button>
+                          </td>
+                        </tr>
+                      </tfoot>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </View>
     );
